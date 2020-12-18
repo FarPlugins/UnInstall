72a8dbeb50375b7ef6b55cf2b9e153ee36c48666
Aleksey Dobrunov <ctapmex@gmail.com>
2020-12-19 00:12:16 +0500

    build rc; remove lang template

---
+++ src/version.hpp
@@ -0,0 +1,20 @@
+#ifdef _WIN64
+#define PLATFORM " x64"
+#elif defined _WIN32
+#define PLATFORM " x86"
+#else
+#define PLATFORM ""
+#endif
+
+#define PLUGIN_VER_MAJOR 1
+#define PLUGIN_VER_MINOR 10
+#define PLUGIN_VER_PATCH 17
+#define PLUGIN_DESC L"UnInstall for Far Manager 3" PLATFORM
+#define PLUGIN_NAME L"UnInstall"
+#define PLUGIN_FILENAME L"UnInstall.dll"
+#define PLUGIN_COPYRIGHT L"https://github.com/FarPlugins/UnInstall"
+
+#define STRINGIZE2(s) #s
+#define STRINGIZE(s) STRINGIZE2(s)
+
+#define PLUGIN_VERSION STRINGIZE(PLUGIN_VER_MAJOR) "." STRINGIZE(PLUGIN_VER_MINOR) "." STRINGIZE(PLUGIN_VER_PATCH)
